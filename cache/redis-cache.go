1722e672d35c0b46d40f585a4c175beffe3deb74
re-Tick <jain.ritik.1001@gmail.com>
2022-09-11 19:11:47 +0530

    enbeds redis client to override method operations
    Signed-off-by: re-Tick <jain.ritik.1001@gmail.com>

--- cache/redis-cache.go
+++ cache/redis-cache.go
@@ -1,10 +1,12 @@
 package cache
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
-	"github.com/go-redis/redis/v7"
+	"github.com/go-redis/redis/v8"
 	"gitlab.com/pragmaticreviews/golang-mux-api/entity"
 )
 
@@ -22,30 +24,63 @@ func NewRedisCache(host string, db int, exp time.Duration) PostCache {
 	}
 }
 
-func (cache *redisCache) getClient() *redis.Client {
-	return redis.NewClient(&redis.Options{
+type keployRedisClient struct {
+	redis.Client
+}
+
+func (krc *keployRedisClient) Process(cmd redis.Cmder) error {
+	fmt.Println("\ninto keploys's redis Process method")
+	return nil
+}
+
+func (krc *keployRedisClient) ProcessContext(ctx context.Context, cmd redis.Cmder) error {
+	fmt.Println("\ninto keploys's redis ProcessContext method")
+	return nil
+}
+
+func (krc *keployRedisClient) Do(ctx context.Context, args ...interface{}) *redis.Cmd {
+	fmt.Println("\ninto keploys's redis Do method")
+	return krc.Client.Do(ctx, args...)
+}
+
+func (krc *keployRedisClient) Get(ctx context.Context, key string) *redis.StringCmd {
+	fmt.Println("\ninto keploys's redis Get method")
+	return krc.Client.Get(ctx, key)
+}
+
+func (krc *keployRedisClient) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.StatusCmd {
+	fmt.Println("\ninto keploys's redis Get method")
+	return krc.Client.Set(ctx, key, value, expiration)
+}
+
+func (cache *redisCache) getClient() *keployRedisClient {
+	client := redis.NewClient(&redis.Options{
 		Addr:     cache.host,
 		Password: "",
 		DB:       cache.db,
 	})
+	return &keployRedisClient{Client: *client}
 }
 
 func (cache *redisCache) Set(key string, post *entity.Post) {
 	client := cache.getClient()
 
+	// client.Process(nil)
 	// serialize Post object to JSON
 	json, err := json.Marshal(post)
 	if err != nil {
 		panic(err)
 	}
 
-	client.Set(key, json, cache.expires*time.Second)
+	client.Set(context.Background(), key, json, cache.expires*time.Second)
 }
 
 func (cache *redisCache) Get(key string) *entity.Post {
 	client := cache.getClient()
 
-	val, err := client.Get(key).Result()
+	// val, err := client.Do("get", key).Result()
+
+	val, err := client.Get(context.Background(), key).Result()
 	if err != nil {
 		return nil
 	}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/go-chi/chi v4.0.3+incompatible
-	github.com/go-redis/redis/v7 v7.0.0-beta.5
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/keploy/go-sdk v0.5.3
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible
 )
